a472e5ed5143b9dad71e0579afb1b34e27eeeb97
Uncle Tallest <tallest@ohmytallest.productions>
2025-10-13 10:11:00 -0500

    update copyright dates in footer

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -37,7 +37,7 @@ function App() {
         </div>
       </main>
       <footer>
-        <p className="copyright">Copyright 2024 -- Jerry W Jackson</p>
+        <p className="copyright">Copyright 2024-2025 -- Jerry W Jackson</p>
         <a href="https://ko-fi.com/uncletallest" target="_blank">
           <img
             src={kofiButton}
